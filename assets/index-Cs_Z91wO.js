a866156a04188024d157423eb1c77c8434f3d15d
dilukshashamal <dilukshashamal@users.noreply.github.com>
2025-06-11 23:34:17 +0000

    deploy: 0cbcfae50e5fc46c49869b4df51eb104f93a99a6

--- assets/index-Cs_Z91wO.js
+++ assets/index-Cs_Z91wO.js
@@ -16,7 +16,7 @@
           <img src="${c}" alt="Kavindu Shamal">
       </div>
       <div class="hero-content">
-        <h1>Kavindu Shamal</h1>
+        <h1>Diluksha Shamal</h1>
         <p class="subtitle">AI Engineer | Bridging AI and Human Cognition</p>
         <a href="./src/assets/resume.pdf" class="btn" download>Download CV</a>
       </div>
