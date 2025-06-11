0cbcfae50e5fc46c49869b4df51eb104f93a99a6
dilukshashamal <dilukshashamal2001@gmail.com>
2025-06-12 05:03:57 +0530

    change name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export const Hero = () => {
           <img src="${profilePic}" alt="Kavindu Shamal">
       </div>
       <div class="hero-content">
-        <h1>Kavindu Shamal</h1>
+        <h1>Diluksha Shamal</h1>
         <p class="subtitle">AI Engineer | Bridging AI and Human Cognition</p>
         <a href="./src/assets/resume.pdf" class="btn" download>Download CV</a>
       </div>
