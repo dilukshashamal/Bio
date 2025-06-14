a8ee6de9872859a25111fe511a5cbf9879d3a310
dilukshashamal <dilukshashamal@users.noreply.github.com>
2025-06-14 12:15:00 +0000

    deploy: 523f5b9b1e736fcca17fe6928c7976f683580fa8

--- assets/index-XRl3S25v.js
+++ assets/index-XRl3S25v.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&r(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function r(e){if(e.ep)return;e.ep=!0;const i=o(e);fetch(e.href,i)}})();const c=()=>`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&r(s)}).observe(document,{childList:!0,subtree:!0});function l(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function r(e){if(e.ep)return;e.ep=!0;const i=l(e);fetch(e.href,i)}})();const c=()=>`
     <header class="header">
       <nav class="nav">
         <a href="#" class="nav-logo"></a>
@@ -146,10 +146,11 @@
         </div>
       </div>
     </section>
-  `,g="/assets/project-1-DjEbsT8m.png",v="/assets/project-2-gry8C-Ug.png",f="/assets/project-3-Bdx30zLG.png",b=()=>`
+  `,g="/assets/project-1-DjEbsT8m.png",v="/assets/project-2-gry8C-Ug.png",f="/assets/project-3-Bdx30zLG.png",b="/assets/project-4-x_Uqw574.png",y="/assets/project-5-M2bsM5fX.png",A=()=>`
     <section id="projects">
       <h2 class="section-title">My Projects</h2>
       <div class="projects-grid">
+
         <div class="project-card">
           <img src="${g}" alt="Project 1">
           <div class="project-card-content">
@@ -158,6 +159,7 @@
             <a href="https://github.com/dilukshashamal/Medical-Data-Extractor_Beta" class="btn" target="_blank">View on GitHub</a>
           </div>
         </div>
+
         <div class="project-card">
           <img src="${v}" alt="Project 2">
           <div class="project-card-content">
@@ -166,6 +168,7 @@
             <a href="https://github.com/dilukshashamal/cumulative-table-design/tree/main" class="btn" target="_blank">View on GitHub</a>
           </div>
         </div>
+
         <div class="project-card">
           <img src="${f}" alt="Project 3">
           <div class="project-card-content">
@@ -174,9 +177,27 @@
             <a href="https://github.com/dilukshashamal/Omdena_RAG_Based_Chat" class="btn" target="_blank">View on GitHub</a>
           </div>
         </div>
+
+        <div class="project-card">
+          <img src="${b}" alt="Project 1">
+          <div class="project-card-content">
+            <h3>Image Generation and Editing System</h3>
+            <p>A modular image generation system integrating ConfiUI for user interaction and Bagel for orchestration, enabling streamlined pipeline control, real-time customization, and efficient neural network-driven image output.</p>
+          </div>
+        </div>
+
+        <div class="project-card">
+          <img src="${y}" alt="Project 1">
+          <div class="project-card-content">
+            <h3>ETL Pipeline Automation</h3>
+            <p>This project sets up an automated ETL pipeline using Apache Airflow and PostgreSQL, managing data extraction, transformation, and loading via modular DAGs, deployed with Docker Compose.</p>
+            <a href="https://github.com/dilukshashamal/Medical-Data-Extractor_Beta" class="btn" target="_blank">View on GitHub</a>
+          </div>
+        </div>
+
       </div>
     </section>
-  `,y=()=>`
+  `,k=()=>`
       <section id="contact">
           <h2 class="section-title">Contact Me</h2>
           <form class="contact-form">
@@ -186,7 +207,7 @@
               <button type="submit" class="btn">Send Message</button>
           </form>
       </section>
-    `,A=()=>`
+    `,S=()=>`
       <footer class="footer">
           <p>&copy; 2025 Diluksha Shamal. All Rights Reserved.</p>
       </footer>
@@ -198,8 +219,8 @@
     ${h()}
     ${p()}
     ${m()}
-    ${b()}
-    ${y()}
+    ${A()}
+    ${k()}
   </main>
-  ${A()}
-`;const n=document.querySelector(".hamburger"),l=document.querySelector(".nav-links");n.addEventListener("click",()=>{n.classList.toggle("active"),l.classList.toggle("active")});document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener("click",function(t){t.preventDefault(),n.classList.remove("active"),l.classList.remove("active"),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})});particlesJS("particles-js",{particles:{number:{value:80,density:{enable:!0,value_area:800}},color:{value:"#8a2be2"},shape:{type:"circle"},opacity:{value:.6,random:!1},size:{value:3,random:!0},line_linked:{enable:!0,distance:150,color:"#8a2be2",opacity:.4,width:1},move:{enable:!0,speed:4,direction:"none",random:!1,straight:!1,out_mode:"out",bounce:!1}},interactivity:{detect_on:"canvas",events:{onhover:{enable:!0,mode:"repulse"},onclick:{enable:!0,mode:"push"}},modes:{repulse:{distance:100},push:{particles_nb:4}}},retina_detect:!0});const k=new IntersectionObserver(a=>{a.forEach(t=>{t.isIntersecting&&t.target.classList.add("visible")})},{threshold:.1});document.querySelectorAll("section").forEach(a=>{a.classList.add("hidden"),k.observe(a)});
+  ${S()}
+`;const n=document.querySelector(".hamburger"),o=document.querySelector(".nav-links");n.addEventListener("click",()=>{n.classList.toggle("active"),o.classList.toggle("active")});document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener("click",function(t){t.preventDefault(),n.classList.remove("active"),o.classList.remove("active"),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})});particlesJS("particles-js",{particles:{number:{value:80,density:{enable:!0,value_area:800}},color:{value:"#8a2be2"},shape:{type:"circle"},opacity:{value:.6,random:!1},size:{value:3,random:!0},line_linked:{enable:!0,distance:150,color:"#8a2be2",opacity:.4,width:1},move:{enable:!0,speed:4,direction:"none",random:!1,straight:!1,out_mode:"out",bounce:!1}},interactivity:{detect_on:"canvas",events:{onhover:{enable:!0,mode:"repulse"},onclick:{enable:!0,mode:"push"}},modes:{repulse:{distance:100},push:{particles_nb:4}}},retina_detect:!0});const j=new IntersectionObserver(a=>{a.forEach(t=>{t.isIntersecting&&t.target.classList.add("visible")})},{threshold:.1});document.querySelectorAll("section").forEach(a=>{a.classList.add("hidden"),j.observe(a)});
